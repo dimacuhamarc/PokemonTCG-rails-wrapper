9d6bd2d0690a13e1bb9cd9240a09c825ef046f6f
Marc Dimacuha <dimacuha.marc@gmail.com>
2024-05-09 02:28:08 +0800

    chore: Add Faraday gem for API requests

---
+++ app/client/pokemontcg/v1/client.rb
@@ -0,0 +1,39 @@
+# app/client/pokemontcg/v1/client.rb
+require 'faraday'
+
+module Pokemontcg
+  module V1
+    class Client
+      BASE_URI = 'https://api.pokemontcg.io/v2/'.freeze
+      API_KEY = Rails.application.credentials.poke_tcg_secret_key
+      ERROR_CODES = {
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        403 => 'Forbidden',
+        404 => 'Not Found'
+      }.freeze
+
+      def initialize
+        @client = Faraday.new(url: BASE_URI) do |faraday|
+          faraday.request :url_encoded
+          faraday.adapter Faraday.default_adapter
+        end
+      end
+
+      def get_card_by_name(name)
+        response = @client.get("cards?q=name:#{name}", { 'X-Api-Key': API_KEY })
+        handle_response(response)
+      end
+
+      private
+
+      def handle_response(response)
+        if response.success?
+          JSON.parse(response.body)
+        else
+          raise Error, "Error: #{ERROR_CODES[response.status]} - #{response.body}"
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  # /
+  get "/" => "posts#index", as: :root
+  # /posts
+
+
 end
